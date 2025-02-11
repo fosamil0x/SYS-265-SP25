b72dde1471919f8fc92a70791dbe9b34ddb358b5
fosamil0x <luc.evans@mymail.champlain.edu>
2025-02-11 22:55:29 +0000

    update as of 2/11/25 for secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -2,37 +2,16 @@
 
 # secure-ssh.sh
 # author: luc
+
 # Creates a new SSH user, generates an RSA key pair, and disables root SSH login.
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/luc/SYS-265-SP25
+sudo cp SYS-265-SP25/linux/public-key/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
 
-# Ensure the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root" >&2
-    exit 1
-fi
 
-# Check if a username is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
-
-NEW_USER="$1"
-SSH_DIR="/home/$NEW_USER/.ssh"
-AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
-
-# Create the new user and their .ssh directory
-useradd -m -s /bin/bash "$NEW_USER"
-mkdir -p "$SSH_DIR"
-chmod 700 "$SSH_DIR"
-chown "$NEW_USER:$NEW_USER" "$SSH_DIR"
-
-# Generate an RSA key pair
-su - "$NEW_USER" -c "ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''"
-
-# Add the public key to authorized_keys
-cat "$SSH_DIR/id_rsa.pub" > "$AUTHORIZED_KEYS"
-chmod 600 "$AUTHORIZED_KEYS"
-chown "$NEW_USER:$NEW_USER" "$AUTHORIZED_KEYS"
 
 # Disable root SSH login
 sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
